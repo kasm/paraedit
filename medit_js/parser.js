ebe51a02ece5ae87d496737d4d43c87908839d27
Dima <rudimvik@gmail.com>
2018-02-11 02:52:44 +0200

    new object reference conception: ls1.0.1 (ls1.p0.y)

--- medit_js/parser.js
+++ medit_js/parser.js
@@ -29,6 +29,22 @@ var Parser = function (doc) {
             var rez = a1[0];
             var func = a2[0];
             var params = a3[0].split(',');
+            var paramsids = []; // 2d array to store parent line for each of params
+            var paramsrefs = [];
+            for (i=0; i<params.length; i++) {
+                paramsids[i] = [];
+                var t = params[i].split('.');
+                for (j=0; j<t.length; j++) {
+                    // if digit then convert to integer
+                    paramsids[i][j] = t[j];
+                };
+                var ref = doc.objs; // actually should consider .ob
+                for (j=0; j<t.length-1; j++) {
+                    ref = ref[t[j]];
+                };
+                // do not foget case when no children, must be something to manage it
+                paramsrefs[i] = [ref, paramsids[i][paramsids[i].length]];
+            }
             var queryParams = this.paramTypesString(doc.objs[rez].mainIds);
             switch (func) {
                 case 'point': doc.pnts[rez] = [this.parseVal(params[0]), this.parseVal(params[1])];
@@ -57,6 +73,9 @@ var Parser = function (doc) {
                     doc.objs[rez].mains[1] = doc.objs[params[1]].ob;
                     doc.objs[rez].func = gl.query;
                     break;
+                case 'add':
+                    doc.objs[rez] = this.obCreateIfNot(id);
+
                 case 'per':
 
 
